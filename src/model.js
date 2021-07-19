bd6b3f1055ce221c432f5038a0c44dcb6a543a9d
Nikita Popov <ningen.tos@gmail.com>
2021-07-19 14:36:19 +0500

    getBooks

---
+++ src/model.js
@@ -0,0 +1,12 @@
+const booksDB = [
+  {
+    title: "Harry Potter and the Philosopher's stone",
+    author: "J.K. Rowling"
+  },
+  {
+    title: "Jurassic Park",
+    author: "Michael Crichton"
+  }
+]
+
+export const getBooks = () => booksDB;
