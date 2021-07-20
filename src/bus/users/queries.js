205f3a19c16d5c7ecfeaed03ff43a4017d744f5b
Nikita Popov <ningen.tos@gmail.com>
2021-07-20 17:08:55 +0500

    Added context to server

--- src/bus/users/queries.js
+++ src/bus/users/queries.js
@@ -1,7 +1,15 @@
 import { db } from './db'
 
 export const queries = {
-  users: (_, __) => {
-    return db;
+  users: (_, __, ctx) => {
+    if (ctx.username) {
+      return db;
+    } else {
+      return db.map(({ name, email }) => ({
+        name,
+        email,
+        password: null
+      }));
+    }
   }
 }
--- src/init/server.js
+++ src/init/server.js
@@ -1,6 +1,9 @@
 // Core
 import express from "express";
 import { ApolloServer } from "apollo-server-express";
+import session from 'express-session';
+import cors from 'cors';
+import { sessionOptions, corsOptions } from "./config";
 
 // Schema Types
 import schema from './types.graphql'
@@ -11,7 +14,14 @@ import { resolvers } from './resolvers'
 // API
 import { api as starshipsAPI } from '../bus/starships/dataSource'
 
+// Middleware
+import { readToken } from "./readToken";
+
 const app = express();
+
+app.use(session(sessionOptions));
+app.use(cors(corsOptions));
+app.use(readToken);
 
 const server = new ApolloServer({
   typeDefs: schema,
@@ -20,6 +30,9 @@ const server = new ApolloServer({
     return {
       starshipsAPI
     }
+  },
+  context: ({ req, res }) => {
+    return { req, res }
   }
 });
 
